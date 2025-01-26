d511af94f0fb167e662af3f1ac32dfb0a3407e2f
unknown <mutafailu@gmail.com>
2025-01-26 20:37:23 +0300

    first commit

---
+++ scrypt.js
@@ -0,0 +1,55 @@
+
+
+
+// Получите элементы
+const toggleButton = document.getElementById('toggleButton');
+const dispElements = document.querySelectorAll('.disp');
+const disp2Elements = document.querySelectorAll('.disp2');
+
+
+// Функция для переключения видимости блоков
+function toggleVisibility() {
+    // Переключаем видимость для блоков с классом 'disp'
+    dispElements.forEach(element => {
+        element.style.display = element.style.display === 'none' ? 'block' : 'none';
+    });
+
+    // Проверяем, если экран планшетный, добавляем переключение для 'disp2'
+    if (window.matchMedia("(max-width: 1024px)").matches) {
+        disp2Elements.forEach(element => {
+            element.style.display = element.style.display === 'none' ? 'block' : 'none';
+           
+        });
+       
+    }
+}
+
+// Изначально скрываем элементы
+dispElements.forEach(element => element.style.display = 'none');
+
+if (window.matchMedia("(max-width: 1024px)").matches) {
+    disp2Elements.forEach(element => element.style.display = 'none');
+   
+}else{
+    disp2Elements.forEach(element => element.style.display = 'block'); 
+}
+
+// Привязываем обработчик событий к кнопке
+toggleButton.addEventListener('click', toggleVisibility);
+
+
+
+
+/*----------------------------------------------------------------------------*/
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+  
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+  });
