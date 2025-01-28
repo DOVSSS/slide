28c6080e771073ab9f234ed33a176189f7ef3312
unknown <mutafailu@gmail.com>
2025-01-29 02:23:15 +0300

    2 commit

--- scrypt.js
+++ scrypt.js
@@ -42,14 +42,6 @@ toggleButton.addEventListener('click', toggleVisibility);
 
 /*----------------------------------------------------------------------------*/
 
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
+
+
   
-    loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-  });
